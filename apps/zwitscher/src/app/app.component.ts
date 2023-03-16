aecd8b494862cafde43186ef68ca8c8f3dddce9e
Gregor Speck <gregor.speck@bridging-it.de>
2023-03-16 08:43:56 +0100

    Initial commit

---
+++ apps/zwitscher/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'enterjs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'zwitscher';
+}
