b6509fb853b1b781e2cf9257dae6108545f9a690
Gregor Speck <gregor.speck@bridging-it.de>
2023-06-19 09:33:45 +0200

    added even more content for test app

---
+++ src/app/shared/index.ts
@@ -0,0 +1,4 @@
+export * from './sidenav/sidenav.component';
+export * from './sidebar/sidebar.component';
+export * from './frame/frame.component';
+export * from './frame/content/content.component';
