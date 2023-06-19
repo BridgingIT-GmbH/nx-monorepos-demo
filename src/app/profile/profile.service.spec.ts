46acde1d321071f7235489010ad4b31e6cf5dfe5
Gregor Speck <gregor.speck@bridging-it.de>
2023-06-19 22:54:15 +0200

    fixed tests

--- src/app/profile/profile.service.spec.ts
+++ src/app/profile/profile.service.spec.ts
@@ -2,13 +2,17 @@ import {TestBed} from '@angular/core/testing';
 
 import {ProfileService} from './profile.service';
 import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('ProfileService', () => {
   let service: ProfileService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient()]
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     });
     service = TestBed.inject(ProfileService);
   });
