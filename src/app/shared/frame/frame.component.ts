490da1e4d92d185417d32f7032bdeb319c7fbc95
Gregor Speck <gregor.speck@bridging-it.de>
2023-06-21 23:57:48 +0200

    removed obsolete imports

--- src/app/shared/frame/frame.component.ts
+++ src/app/shared/frame/frame.component.ts
@@ -1,15 +1,12 @@
 import {Component, Input, Signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ChirpComponent} from '../../chirp';
-import {LoginComponent} from '../../auth';
-import {ProfileHeaderComponent} from '../../profile/profile-header/profile-header.component';
 import {SidenavComponent} from '../sidenav/sidenav.component';
 import {SidebarComponent} from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-frame',
   standalone: true,
-  imports: [CommonModule, ChirpComponent, LoginComponent, ProfileHeaderComponent, SidenavComponent, SidebarComponent],
+  imports: [CommonModule, SidenavComponent, SidebarComponent],
   template: `
     <div class="w-[960px] pt-16 flex-row flex h-full gap-4 min-h-full">
       <div aria-description="left" class="sticky w-[180px]">
